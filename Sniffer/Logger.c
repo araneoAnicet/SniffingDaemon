bb2fdf3d0f90e8715fbfb440ed41493939624c10
araneoAnicet <araneoanicet@gmail.com>
2020-06-19 22:09:20 +0100

    add functions to work with packets array

--- Sniffer/Logger.c
+++ Sniffer/Logger.c
@@ -13,3 +13,42 @@ int save_log(
     fflush(logfile);
     return 0;
 }
+
+int add_log(PacketLog** packet_logs, PacketLog new_packet, int* array_top_index) {
+    int index_of_log = search_log(packet_logs, new_packet, 0, *array_top_index - 1);
+    if (index_of_log == -1) {
+        *packet_logs = (PacketLog*) realloc(packet_logs, sizeof(packet_logs) * 2);
+        (*packet_logs)[*array_top_index] = new_packet;
+        *array_top_index++;
+        return 1;
+    } 
+    (*packet_logs)[index_of_log].amount_of_packets++;
+    return 1;
+}
+
+int search_log(PacketLog* packet_logs, PacketLog searched_packet, int left_bound, int right_bound) { 
+    int middle;
+    if (right_bound >= left_bound) { 
+        middle = left_bound + (right_bound - left_bound) / 2; 
+   
+        if (strcmp(packet_logs[middle].ip, searched_packet.ip) == 0) {
+            return middle;
+        }
+   
+        if (strcpm(packet_logs[middle].ip, searched_packet.ip) > 0) {
+            return binarySearch(packet_logs, searched_packet, left_bound, middle - 1);
+        } 
+        return binarySearch(packet_logs, searched_packet, middle + 1, right_bound); 
+    }
+  
+    // no packets found 
+    return -1; 
+}
+
+int sort_logs(PacketLog* packet_logs, int last_log_index) {
+    qsort(packet_logs, last_log_index + 1, sizeof(PacketLog), (int(*) (const void *, const void *)) compare_logs);
+}
+
+int compare_logs(PacketLog first_log, PacketLog second_log) {
+    return strcmp(first_log.ip, second_log.ip);
+}
--- Sniffer/Logger.h
+++ Sniffer/Logger.h
@@ -1,8 +1,9 @@
 #pragma once
 #include <stdio.h>
+#include <string.h>
 
 
-#define LOG_FILE_NAME "NetworkSniffingLogs.log"
+#define LOG_FILE_NAME "NetworkSniffing.log"
 #define PACKETS_BUFFER_SIZE 1024
 
 typedef struct {
@@ -11,8 +12,6 @@ typedef struct {
     int amount_of_packets;
 } PacketLog;
 
-PacketLog packet_logs[PACKETS_BUFFER_SIZE];
-
 int save_log(
     FILE* logfile,
     char ip_addr[],
@@ -20,3 +19,17 @@ int save_log(
     char* interface_name
 );
 
+// array_top - index where a new log should be added.
+int add_log(PacketLog** packet_logs, PacketLog new_packet, int* array_top_index);
+
+// binary search. Returns an id of the searched packet. Returns -1 if the packet wasn't found.
+// left_bound should be equal to 0, right_bound should be equal to the index of the element at the top
+int search_log(PacketLog* packet_logs, PacketLog searched_packet, int left_bound, int right_bound);
+
+// quick sort. last_log_index should be equal to the index of the last added element
+int sort_logs(PacketLog* packet_logs, int last_log_index);
+
+
+// compares IP addresses of 2 packets. function exists to ensure qsort function works correctly
+// returns the result of strcmp function with IP addresses given as the arguments
+int compare_logs(PacketLog first_log, PacketLog second_log);
