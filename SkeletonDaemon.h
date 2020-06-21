db8aca56083f6cedb3b45e973e191df77d58713e
araneoAnicet <araneoanicet@gmail.com>
2020-06-21 18:10:21 +0100

    create separate files for skeleton

---
+++ SkeletonDaemon.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+static void skeleton_daemon();
---
+++ snifferd.c
@@ -0,0 +1,8 @@
+#include "Sniffer/Sniffer.h"
+#include "Sniffer/Logger.h"
+#include "SkeletonDaemon.h"
+
+int main(int argc, char* argv[]) {
+    
+    return 0;
+}
