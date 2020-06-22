0d3a611eefd658c43eddaa27b5c69cffa1bf79bd
araneoAnicet <araneoanicet@gmail.com>
2020-06-22 02:38:02 +0100

    add stop command

--- UI/CommandLineInterface.c
+++ UI/CommandLineInterface.c
@@ -20,3 +20,24 @@ int start() {
         }
         return 0;  // configuration file does not exist
 }
+
+int stop() {
+    FILE* conf_file = fopen(CONF_FILE, "r");
+        if (conf_file != NULL) {
+            char* line = NULL;
+            size_t len = 0;
+            ssize_t read;
+            int current_line_index = 0;
+            while ((read = getline(&line, &len, conf_file)) != -1) {
+                if (current_line_index == 1) {
+                    if (atoi(line) == 1) {  // process is currently running
+                        return 0;
+                    } else {  // process is not running
+                        return -1;
+                    }
+                }
+                current_line_index++;
+            }
+        }
+        return -1;  // configuration file does not exist
+}
